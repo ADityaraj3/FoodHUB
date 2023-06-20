92d9ff8c74459f976fad336385b295770098c267
Prakarsh-Mathur02 <202001012@daiict.ac.in>
2023-06-20 21:45:24 +0530

    Login Functionality added

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import Layout from "./Layout";
 import Home from "./Home";
 import SearchByImage from "./SearchByImage";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
 
 function App() {
   return (
-    <Router>
+    <UserContextProvider>
+      <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
           <Route path='/SearchByImage' element={<SearchByImage />}/>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage/>}/>
         </Route>
       </Routes>
     </Router>
+    </UserContextProvider>
   );
 }
 
--- FrontEnd/my-app/src/navbar.js
+++ FrontEnd/my-app/src/navbar.js
@@ -3,9 +3,31 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import {useContext, useEffect} from "react";
+import { UserContext } from './UserContext';
 
 export default function Navbars() {
+    const {setUserInfo, userInfo} = useContext(UserContext);
+    useEffect(() => {
+      fetch('http://localhost:4000/profile', {
+        credentials: 'include',
+      }).then(response => {
+        response.json().then(userInfo => {
+          setUserInfo(userInfo);
+        });
+      });
+    }, []);
+
+    function logout(){
+      fetch('http://localhost:4000/logout' , {
+        credentials: 'include',
+        method: 'POST',
+      });
+      setUserInfo(null);
+    }
+
+    const username= userInfo?.username;
+
     return(
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{marginBottom:'1px',paddingBottom:'0px'}}>
       <Container fluid>
@@ -20,7 +42,18 @@ export default function Navbars() {
           </Nav>
         </Navbar.Collapse>
         <Nav className="ml-auto">
-          <Button variant="primary" style={{ background: '#005A9C' }}>Login</Button>
+          {username && (
+            <>
+              <Button variant="primary" style={{ background: '#005A9C' }} onClick={logout}>Logout</Button>
+            </>
+          )}
+          {!username && (
+            <>
+              <Button href='/login' variant="primary" style={{ background: '#005A9C' }}>Login</Button>
+              <Button href='/register' variant="primary" style={{ background: '#005A9C' }}>Register</Button>
+            </>
+          )}
+          
         </Nav>
       </Container>
     </Navbar>
